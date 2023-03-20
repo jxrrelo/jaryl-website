1dffffda65c155245d9ce0dfc1abd30718cd9cc3
jxrrelo <jarylloh1997@gmail.com>
2023-03-20 22:03:59 +0800

    Add navbar and smooth-scroll feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-scroll";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav-bar">
+        <nav className="nav-container-actions">
+          <ul>
+            <li>
+              <Link activeClass="active" smooth spy to="about">
+                  About
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="experience">
+                  Experience
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="projects">
+                  Projects
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="contact">
+                  Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <section id="home">
+        <div className="home-content">
+          <p className="intro">Hi, my name is</p>
+          <p className="name">Jaryl Loh.</p>
+          <p className="motto">Building and securing is my passion.</p>
+          <p className="whoami">I am a software/security engineer based in Singapore,</p>
+          <p className="whoami">focusing in application development and security.</p>
+        </div>
+      </section>
+      <section id="about"><p className="section-header">About</p></section>
+      <section id="experience"><p className="section-header">Experience</p></section>
+      <section id="projects"><p className="section-header">Projects</p></section>
+      <section id="contact"><p className="section-header">Contact</p></section>
     </div>
   );
 }
