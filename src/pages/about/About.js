44a32417dc7b1a8f92d03e32892929e5136ba495
jxrrelo <jarylloh1997@gmail.com>
2023-04-05 15:29:20 +0800

    Update about me content

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -6,8 +6,17 @@ class About extends Component {
         return (
             <div className="about-me-container">
                 <div className="about-me-description">
-                    Hello! My name is Jaryl and I enjoy meddling with things that live on the internet. My interest in software development and cybersecurity.
-                    Outside of work, you may occasionally find me playing football, climbing and traveling.
+                    Hello! My name is Jaryl and I enjoy meddling with things that live on the internet.
+                    <br /><br />
+                    My interest in software development started back in 2017
+                    when I was first introduced to the basics of programming.
+                    While still being engaged in development, I often
+                    participated in CTF competitions since 2020. The
+                    satisfaction from a well-developed product along with the
+                    thrill of breaking insecure programs were the motivations
+                    that have kept me going ever since.
+                    <br /><br />
+                    Outside of work, you may occasionally find me playing football, climbing and traveling! :)
                 </div>
                 <div>
                     <button className="resume-button">Check out my resume!</button>
